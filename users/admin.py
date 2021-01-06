16314400529b29673db26e22236a6e0fb46fe737
Benjamin Jacob Reji <benjaminjacobreji@gmail.com>
2021-01-06 20:47:40 +0400

    feat(core): add user and profile models
    -  New custom User model to fit requirements of unique emails
    and usernames
    -  Add custom Admin layout for new User Model
    -  Add CustomUserManager to aid Custom User model
    -  Update settings.py to use custom User model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+class UserAdminConfig(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name','last_name', 'email', 'username', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, UserAdminConfig)
+admin.site.register(Profile)
