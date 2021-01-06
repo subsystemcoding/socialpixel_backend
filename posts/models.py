e77bfc561783671bbcab2de43304f4f78a6e9e81
Benjamin Jacob Reji <benjaminjacobreji@gmail.com>
2021-01-06 23:49:25 +0400

    refactor(posts): Add ManyToMany relationship
    -  update __str__ method for model Post

--- posts/models.py
+++ posts/models.py
@@ -15,14 +15,14 @@ class Post(models.Model):
     date_created = models.DateTimeField(default=timezone.now)
     caption = models.CharField(max_length=256, blank=True)
     gps_tag = models.CharField(max_length=128, blank=True) # TODO: Update this later on to support GEOLOCATION
-    tagged_users =  ArrayField(models.CharField(max_length=150), blank=True, null=True)
+    tagged_users =  models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='tagged', blank=True)
     upvotes = models.PositiveIntegerField(default=0)
     views = models.PositiveIntegerField(default=0)
     visibility = models.IntegerField(choices=VisibilityEnums.choices, default=VisibilityEnums.PUBLIC)
     media_id = models.UUIDField() # For multiple media use : ArrayField(models.UUIDField())
 
     def __str__(self):
-        return self.author + ':' + self.post_id
+        return str(self.author) + ':' + str(self.post_id)
 
 class Comment(models.Model):
     comment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
