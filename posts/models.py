05766cdab005f730913cb0e5bb57dfc5b0dd3d8a
Benjamin Jacob Reji <benjaminjacobreji@gmail.com>
2021-01-18 02:22:59 +0400

    refactor(posts): update post and comment models

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,3 @@
-import uuid
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
@@ -7,30 +6,48 @@ from django.contrib.postgres.fields import ArrayField
 class Post(models.Model):
     
     class VisibilityEnums(models.IntegerChoices):
-        PUBLIC = 0
-        PRIVATE = 1
+        VISIBLE = 0
+        HIDDEN = 1
 
-    post_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post_id = models.BigAutoField(primary_key=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE,
+        related_name='posted_by',
+    )
     date_created = models.DateTimeField(default=timezone.now)
     caption = models.CharField(max_length=256, blank=True)
     gps_tag = models.CharField(max_length=128, blank=True) # TODO: Update this later on to support GEOLOCATION
-    tagged_users =  models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='tagged', blank=True)
+    tagged_users =  models.ManyToManyField(
+        settings.AUTH_USER_MODEL, 
+        related_name='tagged', 
+        blank=True
+    )
     upvotes = models.PositiveIntegerField(default=0)
     views = models.PositiveIntegerField(default=0)
-    visibility = models.IntegerField(choices=VisibilityEnums.choices, default=VisibilityEnums.PUBLIC)
+    visibility = models.IntegerField(choices=VisibilityEnums.choices, default=VisibilityEnums.VISIBLE)
     media_id = models.UUIDField() # For multiple media use : ArrayField(models.UUIDField())
 
     def __str__(self):
         return str(self.author) + ':' + str(self.post_id)
 
 class Comment(models.Model):
-    comment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment_id = models.BigAutoField(primary_key=True)
+    post_id = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE, 
+        related_name='commented_on'
+    )
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='commented_by')
     comment_content = models.CharField(max_length=256)
-    reply_to_comment = models.UUIDField(blank=True, null=True)
+    reply_to_comment = models.ForeignKey(
+        'self', 
+        on_delete=models.CASCADE, 
+        blank=True, 
+        null=True, 
+        related_name='replies'
+    )
     date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return str(self.post_id) + ':' + str(self.comment_id)
+        return str(self.post_id) + ':' + str(self.author) + ':' + str(self.comment_id)
