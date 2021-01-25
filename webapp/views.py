8a687f4d800d09e8a9922a602132fd166ea0c0c3
Benjamin Jacob Reji <benjaminjacobreji@gmail.com>
2021-01-25 18:39:54 +0400

    feat(webapp): added test home webpage

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from posts.models import Post
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'webapp/home.html', context)
