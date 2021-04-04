30803345697b5b44c8284895c38d55d9af573765
Benjamin Jacob Reji <benjaminjacobreji@gmail.com>
2021-04-04 20:40:54 +0400

    added gps support

--- posts/enums.py
+++ posts/enums.py
@@ -3,3 +3,11 @@ from django.db import models
 class PostVisibilityEnums(models.IntegerChoices):
         ACTIVE = 0
         HIDDEN = 1
+
+class GPSAccuracyEnums(models.IntegerChoices):
+        ONEHUNDREDKILOMETERS = 1
+        TENKILOMETERS = 10
+        ONEKILOMETER = 100
+        ONEHUNDREDMETERS = 1000
+        TENMETERS = 10000
+        ONEMETER = 100000
